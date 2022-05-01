ff1b37b81ddc27a598e918642ba7f014dd517a33
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-05-01 19:17:24 +0900

    ソースの格納ディレクトリを変更

--- Chap02/Practice/ch02-01.c
+++ Chap02/Practice/ch02-01.c
@@ -1,6 +1,6 @@
-/*
- * 最初のCプログラム
- */
+//
+// 最初のCプログラム
+//
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 {
     if (argc == 1)
     {
-        puts("hello world");
+        puts("Hello World!");
     }
     else
     {
