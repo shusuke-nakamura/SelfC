56ecb5eb877314e6b2fde376c8e79295b1a18828
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-05-02 15:47:11 +0900

    2.6のreturn文の学習完了

---
+++ Chap02/Practice/ch02-5q01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int Main(void)
+{
+    puts("Main");
+    return 0;
+}
+
+int main(void)
+{
+    puts("main");
+    return 0;
+}
+
+int MAIN(void)
+{
+    puts("MAIN");
+    return 0;
+}
